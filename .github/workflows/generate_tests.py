ce2dd222d91e8281fb187997060fae20ea291b20
Jonas Pleyer <jonas.dev@pleyer.org>
2025-04-03 22:32:08 +0200

    generate package names depending on the operating system

--- .github/workflows/generate_tests.py
+++ .github/workflows/generate_tests.py
@@ -2,26 +2,32 @@ oss = ["ubuntu-24.04", "ubuntu-22.04", "ubuntu-20.04", "macos-13", "macos-14"]
 toolchains = ["stable", "beta", "nightly"]
 
 
-def os_to_features(os):
-    features = ""
+def os_to_features_packages(os):
     if os == "ubuntu-24.04":
         features = "--features vtk9-1"
+        packages = "libvtk9.1 libvtk9.1-dev"
     elif os == "ubuntu-22.04":
         features = "--features vtk9-1"
+        packages = "libvtk9.1 libvtk9.1-dev"
     elif os == "ubuntu-20.04":
         features = "--features vtk7-0"
+        packages = "libvtk7 libvtk7-dev"
     elif os == "macos-13":
         features = "--features vtk9-4"
+        packages = "vtk"
     elif os == "macos-14":
         features = "--features vtk9-4"
-    return features
+        packages = "vtk"
+    else:
+        raise ValueError("Unknown Operating System")
+    return features, packages
 
 
 for toolchain in toolchains:
     for os in oss:
         os_re = os.replace(".", "_")
         filename = f"test_{toolchain}_{os_re}.yml"
-        features = os_to_features(os)
+        features, packages = os_to_features_packages(os)
         contents = f"""\
 on: [push, pull_request]
 
@@ -33,6 +39,7 @@ jobs:
     with:
       toolchain: {toolchain}
       os: {os}
+      packages: {packages}
       features: {features}
 """
         with open(filename, "w") as f:
@@ -48,5 +55,5 @@ for os in oss:
 actions/workflow/status/jonaspleyer/vtk-rs/\
 test_{toolchain}_{os_re}.yml?style=flat-square&label=CI)"
         line += badge_md + " |"
-    line += f"`cargo build {os_to_features(os)}` |"
+    line += f"`cargo build {os_to_features_packages(os)}` |"
     print(line)
