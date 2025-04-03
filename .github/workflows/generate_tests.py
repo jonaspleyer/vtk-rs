3255107580ed6a4b7472249bfe159dca1a03e402
Jonas Pleyer <jonas.dev@pleyer.org>
2025-04-03 21:15:50 +0200

    change workflows to test for individual ubuntu targets

--- .github/workflows/generate_tests.py
+++ .github/workflows/generate_tests.py
@@ -1,13 +1,26 @@
-oss = ["ubuntu-latest", "macos-13", "macos-14"]
+oss = ["ubuntu-24.04", "ubuntu-22.04", "ubuntu-20.04", "macos-13", "macos-14"]
 toolchains = ["stable", "beta", "nightly"]
+
+
+def os_to_features(os):
+    features = ""
+    if os == "ubuntu-24.04":
+        features = "--features vtk9-1"
+    elif os == "ubuntu-22.04":
+        features = "--features vtk9-1"
+    elif os == "ubuntu-20.04":
+        features = "--features vtk9-1"
+    elif os == "macos-13":
+        features = "--features vtk9-4"
+    elif os == "macos-14":
+        features = "--features vtk9-4"
+    return features
 
 
 for toolchain in toolchains:
     for os in oss:
         filename = "test_{}_{}.yml".format(toolchain, os)
-        features = ""
-        if os == "ubuntu-latest":
-            features = "--features vtk9-1"
+        features = os_to_features(os)
         contents = f"""\
 on: [push, pull_request]
 
@@ -24,8 +37,8 @@ jobs:
         with open(filename, "w") as f:
             f.write(contents)
 
-print("| | " + " | ".join(toolchains) + " |")
-print("|---" * (len(toolchains) + 1) + "|")
+print("| | " + " | ".join(toolchains) + " | Build |")
+print("|---" * (len(toolchains) + 1) + "|---|")
 for os in oss:
     line = f"| `{os}` | "
     for toolchain in toolchains:
@@ -33,4 +46,5 @@ for os in oss:
 actions/workflow/status/jonaspleyer/vtk-rs/\
 test_{toolchain}_{os}.yml?style=flat-square&label=CI)"
         line += badge_md + " |"
+    line += f"`cargo build {os_to_features(os)}` |"
     print(line)
