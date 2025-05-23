c1338e893504886e5cc2f16b85dbd4cd1e989f02
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-23 20:26:43 +0200

    cleanup

--- libvtkrs/vtkSphereSource.cpp
+++ libvtkrs/vtkSphereSource.cpp
@@ -5,27 +5,23 @@
 #include <vtkSphereSource.h>
 
 extern "C" void *sphere_source_new() { return vtkSphereSource::New(); }
+
 extern "C" void sphere_source_delete(void *sphere_source_ptr) {
-    vtkSphereSource *object;
     static_cast<vtkSphereSource *>(sphere_source_ptr)->Delete();
 }
 
 extern "C" void sphere_source_set_radius(void *sphere_source_ptr, double radius) {
-    vtkSphereSource *object;
     static_cast<vtkSphereSource *>(sphere_source_ptr)->SetRadius(radius);
 }
 
 extern "C" double sphere_source_get_radius(void *sphere_source_ptr) {
-    vtkSphereSource *object;
     return static_cast<vtkSphereSource *>(sphere_source_ptr)->GetRadius();
 }
 
 extern "C" void sphere_source_set_center(void *sphere_source_ptr, double center[3]) {
-    vtkSphereSource *object;
     static_cast<vtkSphereSource *>(sphere_source_ptr)->SetCenter(center);
 }
 extern "C" void sphere_source_get_center(void *sphere_source_ptr, double *center) {
-    vtkSphereSource *object;
     double *p = static_cast<vtkSphereSource *>(sphere_source_ptr)->GetCenter();
     memcpy(center, p, 3 * sizeof(double));
 }
@@ -38,7 +34,6 @@ extern "C" void sphere_source_set_theta_resolution(void *sphere_source_ptr, int 
 }
 
 extern "C" const char *sphere_source_print_self(void *sphere_source_ptr, unsigned int indent) {
-    vtkSphereSource *object;
     // This is a cast from the unsigned to signed int.
     // This should be fine
     vtkIndent ind = vtkIndent(indent);
