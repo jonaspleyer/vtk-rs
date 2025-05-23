2745154d9f0484bd0b76eddee9e9771b8400b89a
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-23 20:25:12 +0200

    enable two more functions in testing main function

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,11 @@ fn main() {
     sphere_source.set_radius(5.0);
 
     // Make the surface smooth
-    /* sphere_source.SetPhiResolution(100.);
-    sphere_source.SetThetaResolution(100.);
+    sphere_source.set_phi_resolution(100);
+    sphere_source.set_theta_resolution(100);
 
+    /* let mut mapper = vtk::PolyDataMapper::new();
+    mapper.set_input_connection(sphere_source.get_output_port());
     let mut mapper = vtkPolyDataMapper::New();
     mapper.SetInputConnection(sphere_source.GetOutputPort());
 
