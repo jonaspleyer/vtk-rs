9b627aa5988a68ed8cdb7028f687e484956ff312
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-24 04:19:00 +0200

    remove dummy module

--- libvtkrs/include/cxx_sphere.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "cxx.h"
-#include <memory>
-
-rust::String hello_something(rust::String a);
--- libvtkrs/src/cxx_sphere.cpp
+++
@@ -1,7 +0,0 @@
-#include "cxx.h"
-#include "cxx_sphere.h"
-#include "cxx_sphere.rs.h"
-
-rust::String hello_something(rust::String a) {
-    return a;
-}
