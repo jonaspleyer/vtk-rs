72ac8c42c4cc3faf42f8de1a70bfd5885b95480c
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-25 20:44:56 +0200

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,18 @@
 //!
 //! It depends on system libraries which need to be preinstalled.
 
+// Internal Tools
 mod macros;
+
+use macros::*;
+
+// Exposed API
 mod vtk_named_colors;
 mod vtk_object;
 mod vtk_poly_data_mapper;
 mod vtk_sphere;
 mod vtk_sphere_source;
 
-use macros::*;
 pub use vtk_named_colors::*;
 pub use vtk_object::*;
 pub use vtk_poly_data_mapper::*;
