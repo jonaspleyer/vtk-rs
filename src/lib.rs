f3dac6db02ab1ad34f093d8e6d7606c53ef94b44
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-25 04:00:17 +0200

    fixup! include partial vtkObject wrapper functions

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,14 @@
 
 mod macros;
 mod vtk_named_colors;
+mod vtk_object;
 mod vtk_poly_data_mapper;
 mod vtk_sphere;
 mod vtk_sphere_source;
 
 use macros::*;
 pub use vtk_named_colors::*;
+pub use vtk_object::*;
 pub use vtk_poly_data_mapper::*;
 pub use vtk_sphere::*;
 pub use vtk_sphere_source::*;
