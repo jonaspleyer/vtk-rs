d05e7a08729f7f5f7fe39257540f940c645ffa57
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-26 03:29:59 +0200

    switch CI to new testing and adapt description in README.md

--- .github/workflows/generate_tests.py
+++ .github/workflows/generate_tests.py
@@ -2,29 +2,29 @@ oss = ["ubuntu-24.04", "ubuntu-22.04", "macos-13", "macos-14"]
 toolchains = ["stable", "beta", "nightly"]
 
 
-def os_to_features_packages(os):
+def os_to_envs_packages(os):
     if os == "ubuntu-24.04":
-        features = "--features vtk9-1"
+        environment_vars = "VTK_VERSION=vtk9-1"
         packages = "libvtk9.1 libvtk9-dev"
     elif os == "ubuntu-22.04":
-        features = "--features vtk9-1"
+        environment_vars = "VTK_VERSION=vtk9-1"
         packages = "libvtk9.1 libvtk9-dev"
     elif os == "macos-13":
-        features = "--features vtk9-4"
+        environment_vars = "VTK_VERSION=vtk9-4"
         packages = "vtk"
     elif os == "macos-14":
-        features = "--features vtk9-4"
+        environment_vars = "VTK_VERSION=vtk9-4"
         packages = "vtk"
     else:
         raise ValueError("Unknown Operating System")
-    return features, packages
+    return environment_vars, packages
 
 
 for toolchain in toolchains:
     for os in oss:
         os_re = os.replace(".", "_")
         filename = f"test_{toolchain}_{os_re}.yml"
-        features, packages = os_to_features_packages(os)
+        envs, packages = os_to_envs_packages(os)
         contents = f"""\
 on: [push, pull_request]
 
@@ -37,7 +37,7 @@ jobs:
       toolchain: {toolchain}
       os: {os}
       packages: {packages}
-      features: {features}
+      environment_vars: {envs}
 """
         with open(filename, "w") as f:
             f.write(contents)
@@ -47,12 +47,12 @@ print("|---" * (len(toolchains) + 1) + "|---|")
 for os in oss:
     os_re = os.replace(".", "_")
     line = f"| `{os}` | "
-    features, _ = os_to_features_packages(os)
+    envs, _ = os_to_envs_packages(os)
     for toolchain in toolchains:
         badge_md = f"[![{toolchain}-{os_re}](https://img.shields.io/github/\
 actions/workflow/status/jonaspleyer/vtk-rs/\
 test_{toolchain}_{os_re}.yml?style=flat-square&label=CI)](\
 https://github.com/jonaspleyer/vtk-rs/actions/workflows/test_{toolchain}_{os_re}.yml)"
         line += badge_md + " |"
-    line += f"`cargo build {features}` |"
+    line += f"`{envs} cargo build` |"
     print(line)
