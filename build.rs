4055726d1584b90fd5626eec4159e787f1048656
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-30 12:01:51 +0200

    completely rework build script
    - link more specifically
    - cover various cases more obviously

--- build.rs
+++ build.rs
@@ -14,7 +14,7 @@ macro_rules! log(
     };
 );
 
-fn determine_version_suffix(link_path: &std::path::Path) -> Result<Option<String>> {
+fn find_version_suffix(link_path: &std::path::Path) -> Result<Option<String>> {
     if let Ok(v) = std::env::var("VTK_VERSION") {
         return Ok(Some(v));
     }
@@ -55,21 +55,111 @@ fn build_cmake() {
     println!("cargo:rustc-link-lib=static=vtkrs");
 }
 
-// Collect all paths where to search for libraries
-// Also emits flags to link to said paths
+// Collect all paths of directories where to search for libraries
 fn gather_link_paths() -> Result<Vec<std::path::PathBuf>> {
     let mut link_paths = Vec::<std::path::PathBuf>::new();
 
     if cfg!(unix) {
-        let paths: [std::path::PathBuf; 3] = [
+        link_paths.extend([
             "/usr/lib".into(),
             "/usr/lib/x86_64-linux-gnu/".into(),
             "/usr/local/lib/".into(),
-        ];
-        for p in paths.iter() {
-            println!("cargo:rustc-link-search={}", p.display());
+        ]);
+    }
+
+    // Search in paths where homebrew might install packages
+    if cfg!(unix) || cfg!(target_os = "linux") || cfg!(target_os = "macos") {
+        link_paths.extend([
+            "/usr/local/Cellar/vtk/".into(),
+            "/opt/homebrew/Cellar/vtk".into(),
+        ]);
+        link_paths.extend(
+            glob::glob("/opt/homebrew/lib/*vtk*")?
+                .filter_map(|x| x.ok())
+                .filter(|x| x.is_dir()),
+        );
+    }
+
+    Ok(link_paths)
+}
+
+/// If VTK_VERSION && !VTK_DIR:
+///      => set version_suffix
+///      => Search for matching VTK Version in possible link paths
+///      => Hopefully find something and emit linker vars
+/// If VTK_VERSION && VTK_DIR::
+///      => Simply emit this linker combination
+/// If !VTK_VERSION && VTK_DIR:
+///      => Search for matching VTK Version in given path
+/// If !VTK_VERSION && !VTK_DIR:
+///      => Search in possible link paths for any version of vtk
+///         by inferring version from (lib)vtkCommonCore{version}.(so|lib|...)
+///      => Hopefully find something and emit linker vars
+fn main() -> Result<()> {
+    // Exit early without doing anything if we are building for docsrs
+    if std::env::var("DOCS_RS").is_ok() {
+        return Ok(());
+    }
+
+    if let Ok(val) = std::env::var("VERBOSE") {
+        if val == "1" || val.to_lowercase() == "true" {
+            WARN.store(true, std::sync::atomic::Ordering::Relaxed);
+            log!("-- Verbose Logging Enabled");
         }
-        link_paths.extend(paths);
+    }
+
+    let vtk_version = std::env::var("VTK_VERSION").ok();
+    let vtk_dir = std::env::var("VTK_DIR").ok();
+
+    let version_suffix = match (vtk_version, vtk_dir) {
+        (Some(v), Some(d)) => {
+            if !std::path::PathBuf::from(&d).exists() {
+                return Err(format!("Given Path {d} does not exist in filesystem").into());
+            }
+            println!("cargo:rustc-link-search={d}");
+            v
+        }
+        (None, Some(d)) => {
+            println!("cargo:rustc-link-search={d}");
+            find_version_suffix(&std::path::PathBuf::from(d))?.unwrap_or_default()
+        }
+        (Some(v), None) => {
+            // Search for matching VTK Verison in given path
+            let link_paths = gather_link_paths()?;
+            for link_path in link_paths.into_iter() {
+                let suffix = find_version_suffix(&link_path)?;
+                if suffix.unwrap_or_default() == v {
+                    println!("cargo:rustc-link-search={}", link_path.display());
+                    break;
+                }
+            }
+            v
+        }
+        (None, None) => {
+            let link_paths = gather_link_paths()?;
+            let mut versions: Vec<(std::path::PathBuf, String)> = link_paths
+                .into_iter()
+                .filter_map(|link_path| {
+                    find_version_suffix(&link_path).ok().map(|x| (link_path, x))
+                })
+                .map(|(p, s)| (p, s.unwrap_or_default()))
+                .collect();
+            versions.sort_by_key(|x| x.1.len());
+            if let Some((path, version)) = versions.into_iter().next() {
+                println!("cargo:rustc-link-search={}", path.display());
+                version
+            } else {
+                "".to_string()
+            }
+        }
+    };
+
+    // Build cpp project
+    build_cmake();
+
+    let linker_args_raw = include_str!("linker-args.txt");
+    for line in linker_args_raw.lines() {
+        println!("cargo:rustc-link-lib=dylib={}{}", line, version_suffix);
     }
 
     // Link with c++ standard library
@@ -78,71 +168,6 @@ fn gather_link_paths() -> Result<Vec<std::path::PathBuf>> {
     }
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-link-lib=dylib=c++");
-    }
-
-    // if VTK_DIR is specified, do not try to determine anything else and return
-    if let Ok(v) = std::env::var("VTK_DIR") {
-        println!("cargo:rustc-link-search={v}");
-        link_paths.push(v.into());
-        return Ok(link_paths);
-    }
-
-    // Search in paths where homebrew might install packages
-    if cfg!(unix) || cfg!(target_os = "linux") || cfg!(target_os = "macos") {
-        link_paths.extend([
-            std::path::PathBuf::from("/usr/local/Cellar/vtk/"),
-            std::path::PathBuf::from("/opt/homebrew/Cellar/vtk"),
-        ]);
-        link_paths.extend(glob::glob("/opt/homebrew/lib/*vtk*")?.filter_map(|x| x.ok()));
-    }
-
-    Ok(link_paths)
-}
-
-fn main() -> Result<()> {
-    if let Ok(val) = std::env::var("VERBOSE") {
-        if val == "1" {
-            WARN.store(true, std::sync::atomic::Ordering::Relaxed);
-            log!("Verbose Logging Enabled");
-        }
-    }
-
-    // Exit early without doing anything if we are building for docsrs
-    if std::env::var("DOCS_RS").is_ok() {
-        return Ok(());
-    }
-
-    build_cmake();
-
-    let link_paths = if let Ok(vtk_dir) = std::env::var("VTK_DIR") {
-        println!("cargo:rustc-link-search={vtk_dir}");
-        vec![std::path::PathBuf::from(vtk_dir)]
-    } else {
-        log!("-- Automatically Determine VTK Lib Path");
-        gather_link_paths()?
-    };
-
-    log!("-- Determine Version Suffix");
-    let mut suffixes: Vec<(std::path::PathBuf, String)> = link_paths
-        .into_iter()
-        .flat_map(|x| {
-            determine_version_suffix(&x)
-                .ok()
-                .and_then(|y| y.map(|y| (x, y)))
-        })
-        .collect();
-    log!("{suffixes:?}");
-    suffixes.sort_by_key(|x| x.1.len());
-    log!("{suffixes:?}");
-
-    let (link_path, version_suffix) = suffixes.into_iter().next().unwrap_or_default();
-    if link_path.exists() {
-        println!("cargo:rustc-link-search={}", link_path.display());
-    }
-
-    let linker_args_raw = include_str!("linker-args.txt");
-    for line in linker_args_raw.lines() {
-        println!("cargo:rustc-link-lib=dylib={}{}", line, version_suffix);
     }
 
     Ok(())
