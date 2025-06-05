cd2598b336821892b94c8a454f9fe309b60fbfcb
Jonas Pleyer <jonas.dev@pleyer.org>
2025-06-05 18:17:02 +0200

    add parsing tests

--- vtk-wrap/src/main.rs
+++ vtk-wrap/src/main.rs
@@ -233,7 +233,270 @@ struct File {
     name: String,
     #[serde(rename = "class")]
     #[serde(default = "Vec::new")]
-    classes: Vec<CClass>,
+    classes: Vec<Class>,
+}
+
+#[cfg(test)]
+mod test_parsing {
+    use super::*;
+
+    const METHOD: &str = r#"
+<method
+    name="GetClassNameInternal"
+    property="ClassNameInternal"
+    access="protected"
+    const="1"
+    virtual="1"
+>
+    <signature>
+            virtual const char *GetClassNameInternal() const
+    </signature>
+    <comment>
+            Return the class name as a string. This method is overridden
+            in all subclasses of vtkObjectBase with the vtkTypeMacro found
+            in vtkSetGet.h.
+    </comment>
+    <return type="const char" pointer="*" />
+</method>"#;
+
+    #[test]
+    fn parse_method() -> Result<()> {
+        let Method {
+            name,
+            property,
+            access,
+            is_const,
+            is_virtual,
+            signature,
+            comment,
+            return_type,
+        } = serde_xml_rs::SerdeXml::new()
+            .overlapping_sequences(true)
+            .from_str(METHOD)?;
+        assert_eq!(name, "GetClassNameInternal");
+        assert_eq!(property.unwrap(), "ClassNameInternal");
+        assert_eq!(access, Some(Access::Protected));
+        assert_eq!(is_const, Some(1));
+        assert_eq!(is_virtual, Some(1));
+        assert_eq!(
+            signature,
+            "virtual const char *GetClassNameInternal() const"
+        );
+        assert_eq!(
+            comment.unwrap(),
+            "\
+            Return the class name as a string. This method is overridden
+            in all subclasses of vtkObjectBase with the vtkTypeMacro found
+            in vtkSetGet.h."
+        );
+        assert_eq!(
+            return_type,
+            Some(ReturnType {
+                ret_type: "const char".into(),
+                pointer: Some(Pointer::Star)
+            })
+        );
+
+        Ok(())
+    }
+
+    const PROPERTY: &str = r#"
+<property name="ClassNameInternal" access="protected" type="char" pointer="*">
+    <comment>
+            Return the class name as a string. This method is overridden
+            in all subclasses of vtkObjectBase with the vtkTypeMacro found
+            in vtkSetGet.h.
+    </comment>
+    <methods bitfield="GET" access="protected" />
+</property>"#;
+
+    #[test]
+    fn parse_property() -> Result<()> {
+        let Property {
+            name,
+            access,
+            r#type,
+            pointer,
+            comment,
+            methods,
+        } = serde_xml_rs::SerdeXml::new()
+            .overlapping_sequences(true)
+            .from_str(PROPERTY)?;
+
+        assert_eq!(name, "ClassNameInternal");
+        assert_eq!(access, Access::Protected);
+        assert_eq!(r#type, "char");
+        assert_eq!(pointer, Some(Pointer::Star));
+        assert_eq!(
+            comment.unwrap(),
+            "\
+            Return the class name as a string. This method is overridden
+            in all subclasses of vtkObjectBase with the vtkTypeMacro found
+            in vtkSetGet.h."
+        );
+        assert_eq!(
+            methods,
+            vec![PropertyMethods {
+                bitfield: "GET".into(),
+                access: Access::Protected
+            }]
+        );
+
+        Ok(())
+    }
+
+    const CLASS: &str = r#"
+<class name="vtkChart" abstract="1">
+  <comment>
+     @brief   Factory class for drawing 2D charts
+
+
+     This defines the interface for a chart.
+  </comment>
+  <base name="vtkContextItem" access="public" />
+
+  <inheritance>
+    <context name="vtkContextItem" access="public" />
+    <context name="vtkAbstractContextItem" access="public" />
+    <context name="vtkObject" access="public" />
+    <context name="vtkObjectBase" access="public" />
+  </inheritance>
+
+  <property name="ClassNameInternal" access="protected" type="char" pointer="*">
+    <comment>
+       Return the class name as a string. This method is overridden
+       in all subclasses of vtkObjectBase with the vtkTypeMacro found
+       in vtkSetGet.h.
+    </comment>
+    <methods bitfield="GET" access="protected" />
+  </property>
+
+  <method name="GetClassNameInternal" property="ClassNameInternal" access="protected" const="1" virtual="1">
+    <signature>
+       virtual const char *GetClassNameInternal() const
+    </signature>
+    <comment>
+       Return the class name as a string. This method is overridden
+       in all subclasses of vtkObjectBase with the vtkTypeMacro found
+       in vtkSetGet.h.
+    </comment>
+    <return type="const char" pointer="*" />
+  </method>
+
+  <typedef name="Superclass" access="public" type="vtkContextItem" />
+</class>"#;
+
+    #[test]
+    fn parse_class() -> Result<()> {
+        let Class {
+            name,
+            access,
+            is_abstract,
+            comment,
+            base,
+            inheritance,
+            methods,
+            typedefs,
+            properties,
+            members,
+        } = serde_xml_rs::SerdeXml::new()
+            .overlapping_sequences(true)
+            .from_str(CLASS)?;
+
+        assert_eq!(name, "vtkChart");
+        assert_eq!(access, None);
+        assert_eq!(is_abstract, Some(1));
+        assert_eq!(
+            comment.unwrap(),
+            "\
+     @brief   Factory class for drawing 2D charts
+
+
+     This defines the interface for a chart."
+        );
+        assert_eq!(
+            base,
+            vec![Base {
+                name: "vtkContextItem".into(),
+                access: Access::Public
+            }]
+        );
+        assert_eq!(
+            inheritance.unwrap(),
+            Inheritance {
+                context: vec![
+                    CContext {
+                        name: "vtkContextItem".into(),
+                        access: Access::Public
+                    },
+                    CContext {
+                        name: "vtkAbstractContextItem".into(),
+                        access: Access::Public
+                    },
+                    CContext {
+                        name: "vtkObject".into(),
+                        access: Access::Public
+                    },
+                    CContext {
+                        name: "vtkObjectBase".into(),
+                        access: Access::Public
+                    },
+                ]
+            }
+        );
+        assert_eq!(
+            methods,
+            vec![Method {
+                name: "GetClassNameInternal".into(),
+                property: Some("ClassNameInternal".into()),
+                access: Some(Access::Protected),
+                is_const: Some(1),
+                signature: "virtual const char *GetClassNameInternal() const".into(),
+                is_virtual: Some(1),
+                comment: Some(
+                    "\
+       Return the class name as a string. This method is overridden
+       in all subclasses of vtkObjectBase with the vtkTypeMacro found
+       in vtkSetGet.h."
+                        .into(),
+                ),
+                return_type: Some(ReturnType {
+                    ret_type: "const char".into(),
+                    pointer: Some(Pointer::Star),
+                }),
+            }]
+        );
+        assert_eq!(
+            typedefs,
+            vec![TypeDef {
+                name: "Superclass".into(),
+                access: Access::Public,
+                r#type: "vtkContextItem".into()
+            }]
+        );
+        assert_eq!(
+            properties,
+            vec![Property {
+                name: "ClassNameInternal".into(),
+                access: Access::Protected,
+                r#type: "char".into(),
+                pointer: Some(Pointer::Star),
+                comment: Some(
+                    "\
+       Return the class name as a string. This method is overridden
+       in all subclasses of vtkObjectBase with the vtkTypeMacro found
+       in vtkSetGet.h."
+                        .into()
+                ),
+                methods: vec![PropertyMethods {
+                    bitfield: "GET".into(),
+                    access: Access::Protected
+                }]
+            }]
+        );
+
+        Ok(())
+    }
 }
 
 fn main() -> Result<()> {
