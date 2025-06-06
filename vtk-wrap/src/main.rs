1740b6310d313272d371f7f9d731df375ccd6165
Jonas Pleyer <jonas.dev@pleyer.org>
2025-06-06 23:38:30 +0200

    print out generated code

--- vtk-wrap/src/main.rs
+++ vtk-wrap/src/main.rs
@@ -8,21 +8,13 @@ use parsing::*;
 
 fn main() -> Result<()> {
     // Obtain all modules
-    let modules = get_modules(&std::path::PathBuf::from_iter(["WrapVTK", "build", "xml"]))?;
+    let modules = get_modules("WrapVTK/build/xml/*ChartsCore")?;
 
-    use rayon::prelude::*;
-    modules
-        .into_par_iter()
-        .map(|module| {
-            for file in module.files.iter() {
-                for class in file.1.classes.iter() {
-                    let wrapper = gen_wrapper(class)?;
-                    println!("{wrapper}");
-                }
-            }
-            Ok(())
-        })
-        .collect::<Result<Vec<_>>>()?;
+    let file = &modules[0].files[0].1;
+    for class in file.classes.iter() {
+        let wrapper = gen_wrapper(class)?;
+        println!("{wrapper}");
+    }
 
     Ok(())
 }
