93fa023f5e3f7c2dcb5e5e6f94c4e83394288f48
Jonas Pleyer <jonas.dev@pleyer.org>
2025-08-19 23:49:15 +0200

    rename field; implement conversion

--- vtk-wrap/src/intermediate.rs
+++ vtk-wrap/src/intermediate.rs
@@ -11,10 +11,52 @@ pub struct RustMethod {
     pub args: Vec<(crate::parse_cpp::Ident, CppType)>,
 }
 
+impl TryFrom<crate::Method> for RustMethod {
+    type Error = anyhow::Error;
+
+    fn try_from(value: crate::Method) -> Result<Self> {
+        use crate::parse_cpp::Parse;
+        let return_type = if let Some(crate::ReturnType { ret_type, pointer }) = value.return_type {
+            let inner_ty = CppType::parse(&ret_type)?;
+            match pointer {
+                Some(crate::Pointer::Ref) => CppType::Ref(Box::new(inner_ty)),
+                Some(crate::Pointer::Star) => CppType::Pointer(Box::new(inner_ty)),
+                Some(crate::Pointer::StarStar) => {
+                    CppType::Pointer(Box::new(CppType::Ref(Box::new(inner_ty))))
+                }
+                None => inner_ty,
+            }
+        } else {
+            CppType::Void
+        };
+
+        let args = value
+            .parameters
+            .into_iter()
+            .enumerate()
+            .map(|(n, param)| {
+                let name = match param.name {
+                    Some(name) => name,
+                    // TODO make this better
+                    None => format!("p{n}"),
+                };
+                let name = crate::parse_cpp::Ident::parse(&name)?;
+                CppType::parse(&param.r#type).map(|x| (name, x))
+            })
+            .collect::<Result<Vec<_>>>()?;
+
+        Ok(RustMethod {
+            name: value.name,
+            return_type,
+            args,
+        })
+    }
+}
+
 pub struct RustStruct {
     pub name: String,
     pub parents: Vec<ClassName>,
-    pub public_methods: Vec<RustMethod>,
+    pub exposable_methods: Vec<RustMethod>,
 }
 
 pub struct RustModule {
@@ -30,7 +72,7 @@ impl RustModule {
             .into_iter()
             .flat_map(|(_, file)| file.classes)
             .map(|class| {
-                (
+                Ok((
                     class.name.clone(),
                     RustStruct {
                         name: class.name.clone(),
@@ -39,11 +81,15 @@ impl RustModule {
                             .into_iter()
                             .cloned()
                             .collect(),
-                        public_methods: vec![],
+                        exposable_methods: class_hierarchy
+                            .get_exposable_methods(&class.name)?
+                            .into_iter()
+                            .map(RustMethod::try_from)
+                            .collect::<Result<Vec<_>>>()?,
                     },
-                )
+                ))
             })
-            .collect();
+            .collect::<Result<HashMap<_, _>>>()?;
 
         Ok(RustModule {
             name,
