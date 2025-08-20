05840929b15702c3bb96aaeae625d31758ead045
Jonas Pleyer <jonas.dev@pleyer.org>
2025-08-20 02:18:29 +0200

    cleanup

--- vtk-wrap/src/main.rs
+++ vtk-wrap/src/main.rs
@@ -26,41 +26,5 @@ fn main() -> Result<()> {
         println!("{}", quote::quote!(#rm));
     }
 
-    // let generator = Generator::new(&modules)?;
-
-    /* for module in modules.iter() {
-        for (_, file) in module.files.iter() {
-            for class in file.classes.iter() {
-                for method in class
-                    .methods
-                    .public
-                    .iter()
-                    .filter(|m| m.access == Access::Public && !m.signature.contains("template"))
-                {
-                    if let Some(ReturnType { ret_type, pointer }) = &method.return_type {
-                        let ty = CppType::parse(ret_type)?;
-                        match pointer {
-                            Some(Pointer::Ref) => println!("&{:?}", CppType::Ref(Box::new(ty))),
-                            Some(Pointer::Star) => println!("{:?}", CppType::Pointer(Box::new(ty))),
-                            Some(Pointer::StarStar) => println!(
-                                "{:?}",
-                                CppType::Pointer(Box::new(CppType::Pointer(Box::new(ty))))
-                            ),
-                            None => println!("{ty:?}"),
-                        }
-                    }
-                    for param in &method.parameters {
-                        let Parameter {
-                            name,
-                            r#type,
-                            reference,
-                        } = param;
-                        let ty = CppType::parse(r#type)?;
-                    }
-                }
-            }
-        }
-    }*/
-
     Ok(())
 }
